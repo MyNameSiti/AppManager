1c92b2ca27786be145a664d36ab33393663aad54
DESKTOP-3DOR5G7\Admin <badang1703@gmail.com>
2024-03-11 22:40:23 +0700

    first commit

---
+++ PhanMenQuanLy/Login.Designer.cs
@@ -0,0 +1,186 @@
+﻿namespace PhanMenQuanLy
+{
+    partial class fmLogin
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(fmLogin));
+            this.pnMain = new DevExpress.XtraEditors.PanelControl();
+            this.pnLogin = new DevExpress.XtraEditors.PanelControl();
+            this.pnLogo = new DevExpress.XtraEditors.PanelControl();
+            this.imgLogo = new DevExpress.XtraEditors.SvgImageBox();
+            this.lblUserName = new DevExpress.XtraEditors.LabelControl();
+            this.lblPassword = new DevExpress.XtraEditors.LabelControl();
+            this.textEdit1 = new DevExpress.XtraEditors.TextEdit();
+            this.textEdit2 = new DevExpress.XtraEditors.TextEdit();
+            this.btnLogin = new DevExpress.XtraEditors.SimpleButton();
+            ((System.ComponentModel.ISupportInitialize)(this.pnMain)).BeginInit();
+            this.pnMain.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pnLogin)).BeginInit();
+            this.pnLogin.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pnLogo)).BeginInit();
+            this.pnLogo.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.imgLogo)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.textEdit1.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.textEdit2.Properties)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pnMain
+            // 
+            this.pnMain.AutoSize = true;
+            this.pnMain.Controls.Add(this.pnLogin);
+            this.pnMain.Controls.Add(this.pnLogo);
+            this.pnMain.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pnMain.Location = new System.Drawing.Point(0, 0);
+            this.pnMain.Name = "pnMain";
+            this.pnMain.Size = new System.Drawing.Size(496, 179);
+            this.pnMain.TabIndex = 0;
+            // 
+            // pnLogin
+            // 
+            this.pnLogin.Controls.Add(this.btnLogin);
+            this.pnLogin.Controls.Add(this.textEdit2);
+            this.pnLogin.Controls.Add(this.textEdit1);
+            this.pnLogin.Controls.Add(this.lblPassword);
+            this.pnLogin.Controls.Add(this.lblUserName);
+            this.pnLogin.Dock = System.Windows.Forms.DockStyle.Right;
+            this.pnLogin.Location = new System.Drawing.Point(169, 2);
+            this.pnLogin.Name = "pnLogin";
+            this.pnLogin.Size = new System.Drawing.Size(325, 175);
+            this.pnLogin.TabIndex = 1;
+            // 
+            // pnLogo
+            // 
+            this.pnLogo.Controls.Add(this.imgLogo);
+            this.pnLogo.Dock = System.Windows.Forms.DockStyle.Left;
+            this.pnLogo.Location = new System.Drawing.Point(2, 2);
+            this.pnLogo.Name = "pnLogo";
+            this.pnLogo.Size = new System.Drawing.Size(161, 175);
+            this.pnLogo.TabIndex = 0;
+            // 
+            // imgLogo
+            // 
+            this.imgLogo.AutoSizeInLayoutControl = true;
+            this.imgLogo.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.imgLogo.Location = new System.Drawing.Point(2, 2);
+            this.imgLogo.Name = "imgLogo";
+            this.imgLogo.Size = new System.Drawing.Size(157, 171);
+            this.imgLogo.SizeMode = DevExpress.XtraEditors.SvgImageSizeMode.Stretch;
+            this.imgLogo.SvgImage = ((DevExpress.Utils.Svg.SvgImage)(resources.GetObject("imgLogo.SvgImage")));
+            this.imgLogo.TabIndex = 0;
+            this.imgLogo.Text = "svgImageBox1";
+            // 
+            // lblUserName
+            // 
+            this.lblUserName.Appearance.Font = new System.Drawing.Font("Century Gothic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblUserName.Appearance.Options.UseFont = true;
+            this.lblUserName.Location = new System.Drawing.Point(5, 28);
+            this.lblUserName.Name = "lblUserName";
+            this.lblUserName.Size = new System.Drawing.Size(130, 30);
+            this.lblUserName.TabIndex = 0;
+            this.lblUserName.Text = "User Name";
+            // 
+            // lblPassword
+            // 
+            this.lblPassword.Appearance.Font = new System.Drawing.Font("Century Gothic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPassword.Appearance.Options.UseFont = true;
+            this.lblPassword.Location = new System.Drawing.Point(5, 86);
+            this.lblPassword.Name = "lblPassword";
+            this.lblPassword.Size = new System.Drawing.Size(109, 30);
+            this.lblPassword.TabIndex = 1;
+            this.lblPassword.Text = "Password";
+            // 
+            // textEdit1
+            // 
+            this.textEdit1.Location = new System.Drawing.Point(141, 25);
+            this.textEdit1.Name = "textEdit1";
+            this.textEdit1.Properties.Appearance.Font = new System.Drawing.Font("Century Gothic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textEdit1.Properties.Appearance.Options.UseFont = true;
+            this.textEdit1.Size = new System.Drawing.Size(173, 36);
+            this.textEdit1.TabIndex = 2;
+            // 
+            // textEdit2
+            // 
+            this.textEdit2.Location = new System.Drawing.Point(141, 83);
+            this.textEdit2.Name = "textEdit2";
+            this.textEdit2.Properties.Appearance.Font = new System.Drawing.Font("Century Gothic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textEdit2.Properties.Appearance.Options.UseFont = true;
+            this.textEdit2.Size = new System.Drawing.Size(173, 36);
+            this.textEdit2.TabIndex = 3;
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.Appearance.Font = new System.Drawing.Font("Tahoma", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLogin.Appearance.Options.UseFont = true;
+            this.btnLogin.Location = new System.Drawing.Point(62, 125);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(120, 40);
+            this.btnLogin.TabIndex = 4;
+            this.btnLogin.Text = "Login";
+            // 
+            // fmLogin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(496, 179);
+            this.Controls.Add(this.pnMain);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "fmLogin";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Login";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.fmLogin_FormClosing);
+            ((System.ComponentModel.ISupportInitialize)(this.pnMain)).EndInit();
+            this.pnMain.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pnLogin)).EndInit();
+            this.pnLogin.ResumeLayout(false);
+            this.pnLogin.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pnLogo)).EndInit();
+            this.pnLogo.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.imgLogo)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.textEdit1.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.textEdit2.Properties)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private DevExpress.XtraEditors.PanelControl pnMain;
+        private DevExpress.XtraEditors.PanelControl pnLogin;
+        private DevExpress.XtraEditors.PanelControl pnLogo;
+        private DevExpress.XtraEditors.SvgImageBox imgLogo;
+        private DevExpress.XtraEditors.LabelControl lblUserName;
+        private DevExpress.XtraEditors.TextEdit textEdit2;
+        private DevExpress.XtraEditors.TextEdit textEdit1;
+        private DevExpress.XtraEditors.LabelControl lblPassword;
+        private DevExpress.XtraEditors.SimpleButton btnLogin;
+    }
+}
---
+++ PhanMenQuanLy/Main.cs
@@ -0,0 +1,22 @@
+﻿using DevExpress.XtraBars;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace PhanMenQuanLy
+{
+    public partial class Main : DevExpress.XtraBars.FluentDesignSystem.FluentDesignForm
+    {
+        public Main()
+        {
+            InitializeComponent();
+            fmLogin fmLogin = new fmLogin();
+            fmLogin.ShowDialog();
+        }
+    }
+}
