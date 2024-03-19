be833c7ef564bc49a28bf909149523cef9372bd2
Siti <badang1703@gmail.com>
2024-03-19 13:49:19 +0700

    them ham quan ly ngon ngu

---
+++ PhanMenQuanLy/ResourceHelper.cs
@@ -0,0 +1,23 @@
+﻿using System.Globalization;
+using System.Resources;
+
+namespace PhanMenQuanLy
+{
+    public class ResourceHelper
+    {
+        private static ResourceManager _resourceManager;
+
+        static ResourceHelper()
+        {
+            _resourceManager = new ResourceManager("PhanMenQuanLy.Resource", typeof(ResourceHelper).Assembly);
+        }
+
+        public static string GetString(string key, CultureInfo culture = null)
+        {
+            if (culture == null)
+                culture = CultureInfo.CurrentUICulture;
+
+            return _resourceManager.GetString(key, culture);
+        }
+    }
+}
